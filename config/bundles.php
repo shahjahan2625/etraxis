c7b3c5b70aa8ac67f40efb251e24595f99e7b4bc
Artem Rodygin <artem.rodygin@gmail.com>
2021-07-11 18:31:44 +1200

    Security

--- config/bundles.php
+++ config/bundles.php
@@ -7,6 +7,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class                => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class                        => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class                    => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class                  => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                          => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class            => ['dev' => true, 'test' => true],
 ];
